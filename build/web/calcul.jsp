b9ad52d1bb1c511448c9b3949b9e7d7544e5d63c
Djamel <djamelbourourou@gmail.com>
2025-06-17 12:30:24 +0200

    deuxieme version

--- build/web/calcul.jsp
+++ build/web/calcul.jsp
@@ -9,9 +9,9 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>JSP Page1</title>
     </head>
     <body>
-          <h1>${m}</h1>
+          <h1> la moyenne est ${m}</h1>
     </body>
 </html>
---
+++ test/model/EtudiantTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pc
+ */
+public class EtudiantTest {
+    
+    public EtudiantTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of calcul method, of class Etudiant.
+     */
+    @Test
+    public void testCalcul() {
+        System.out.println("calcul");
+        int not1 = 0;
+        int not2 = 0;
+        Etudiant instance = null;
+        float expResult = 0.0F;
+        float result = instance.calcul(not1, not2);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
